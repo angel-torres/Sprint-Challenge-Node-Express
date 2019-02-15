ab8d957cea259a9008073675ebc3e5fd4812b63a
Angel Torres <alas4224@gmail.com>
2019-02-15 11:23:17 -0600

    added a few CRUD operations to projects route

--- projects/projects-router.js
+++ projects/projects-router.js
@@ -11,4 +11,26 @@ router.get('/', async (req, res) => {
     }
 })
 
+router.get('/:id', async (req, res) => {
+    const id = req.params.id
+    console.log(id)
+    try {
+        const response = await Projects.get(id)
+        res.status(200).json(response)
+    } 
+    catch {
+        res.status(500).json({errorMessage:"Could not retreive project."})
+    }
+})
+
+router.get('/:projectId/actions', async (req, res) => {
+    const projectId = req.params.projectId
+    try {
+        const projectActions = await Projects.getProjectActions(projectId);
+        res.status(200).json(projectActions)
+    } catch {
+        res.status(500).json({errorMessage:"Could not retreive project actions."})
+    }
+})
+
 module.exports = router;
